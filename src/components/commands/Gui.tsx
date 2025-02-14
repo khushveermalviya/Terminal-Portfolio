939888a375d7e79365a379d2347a0dfc16f04536
Khushveer malviya <115608753+khushveermalviya@users.noreply.github.com>
2025-02-14 22:08:06 +0530

    Update Gui.tsx

--- src/components/commands/Gui.tsx
+++ src/components/commands/Gui.tsx
@@ -10,7 +10,7 @@ const Gui: React.FC = () => {
 
   /* ===== check current command makes redirect ===== */
   if (rerender && currentCommand[0] === "gui") {
-    window.open("https://khushveermalviya.live", "_blank");
+   window.open("https://portfolio-react-khushveer.vercel.app", "_blank");
   }
 
   return <span></span>;
